26faf42f0b8d6b9c658728e758fc71b70479ddc0
Priyachi2003 <96510349+Priyachi2003@users.noreply.github.com>
2022-03-04 16:57:16 +0000

    p5final;

--- p4final.c
+++ p4final.c
@@ -1,1 +1,31 @@
-
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  return a;
+}
+int find_fibo(int n)
+{
+  int a,b,c;
+  c=a+b;
+  a=b;
+  c=b;
+  return c;
+}
+int output(int n)
+{int c;
+  for(int i=3;i<=n;i++)
+  printf("%d is the fibonacci sequence\n",&c);
+  
+  
+}
+int main()
+{
+  int a,n,fibo;
+  a=input();
+  find_fibo(n);
+  output(n);
+  return 0;
+}
